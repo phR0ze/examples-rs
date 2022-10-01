44496f5c682c06b71e02c995c032c3d6a44b44ad
phR0ze <prc451@gmail.com>
2022-10-01 13:32:29 -0600

    Minimal macroquad group stack-overflow example

---
+++ macroquad_ui/group/examples/stack_overflow.rs
@@ -0,0 +1,35 @@
+use core::prelude::*;
+use macroquad::ui::Drag;
+
+fn main_conf() -> Conf {
+    Conf {
+        window_title: "test".to_string(),
+        window_width: 450,
+        window_height: 800,
+        high_dpi: true,
+        ..Default::default()
+    }
+}
+
+pub fn test<F: FnOnce(&mut Ui)>(ui: &mut Ui, size: Vec2, f: F) -> Drag {
+    widgets::Group::new(hash!(), size).ui(ui, f)
+}
+
+#[macroquad::main(main_conf)]
+async fn main() {
+    loop {
+        clear_background(WHITE);
+
+        let size1 = vec2(300., 300.);
+        let size2 = vec2(250., 250.);
+
+        test(&mut *root_ui(), size1, |ui| {
+            test(ui, size2, |ui2| {});
+        });
+        // widgets::Group::new(hash!(), size1).ui(&mut *root_ui(), |ui| {
+        //     widgets::Group::new(hash!(), size2).ui(ui, |ui| {});
+        // });
+
+        next_frame().await
+    }
+}
