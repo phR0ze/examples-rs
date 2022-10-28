50b01e155efb3a5f422390ae88ef8f3435ab1731
phR0ze <prc451@gmail.com>
2022-10-28 13:26:36 -0600

    Implemented parent reference

--- macroquad_ui/core/src/layout.rs
+++ macroquad_ui/core/src/layout.rs
@@ -31,44 +31,58 @@
 //! When updating a layout's position the layouts within that layout will have their positions
 //! updated to match their parent's relative position.
 use crate::prelude::*;
+use std::{cell::RefCell, rc::Rc};
+
+/// Convenience type for shared interior mutable inner Layout object
+pub type SharedLayout = Rc<RefCell<LayoutInner>>;
+
+// Internal implemenation detail for sharing ownership of layouts
+#[derive(Clone, Debug, PartialEq)]
+pub struct LayoutInner {
+    id: String,          // layout identifier
+    x: f32,              // marks start of free horizontal space in the region
+    y: f32,              // marks start of free vertical space in the rgion
+    pos: Vec2,           // position of the layout region excluding margins
+    size: Vec2,          // size of the layout region excluding margins
+    fill_w: bool,        // fill width of layout
+    fill_h: bool,        // fill height of layout
+    expand: bool,        // layout expands to track all content allocated
+    align: Align,        // alignment in the parent layout
+    mode: LayoutMode,    // layout mode directive
+    spacing: f32,        // space to include between widgets
+    margins: RectOffset, // space outside the frame edge
+}
 
 /// Layout describes a region of space and provides mechanisms for calculating where and how a
 /// widget should draw itself inside that region of space. Layout region space allocated to widgets
 /// is then tracked.
 #[derive(Clone, Debug, PartialEq)]
 pub struct Layout {
-    id: String,           // layout identifier
-    x: f32,               // marks start of free horizontal space in the region
-    y: f32,               // marks start of free vertical space in the rgion
-    pos: Vec2,            // position of the layout region excluding margins
-    size: Vec2,           // size of the layout region excluding margins
-    fill_w: bool,         // fill width of layout
-    fill_h: bool,         // fill height of layout
-    expand: bool,         // layout expands to track all content allocated
-    align: Align,         // alignment in the parent layout
-    mode: LayoutMode,     // layout mode directive
-    spacing: f32,         // space to include between widgets
-    margins: RectOffset,  // space outside the frame edge
-    layouts: Vec<Layout>, // child layouts to track
+    layout: SharedLayout,         // layout
+    layouts: Vec<Layout>,         // child layouts
+    parent: Option<SharedLayout>, // parent layout
 }
 
 impl Layout {
     /// Create the default layout
     pub fn new<T: AsRef<str>>(id: T) -> Self {
         Self {
-            id: id.as_ref().to_string(),
-            x: 0.,
-            y: 0.,
-            pos: Vec2::default(),
-            size: Vec2::default(),
-            fill_w: false,
-            fill_h: false,
-            expand: true, // enable expansion by default
-            mode: LayoutMode::default(),
-            align: Align::default(),
-            spacing: 0.,
-            margins: RectOffset::default(),
+            layout: Rc::new(RefCell::new(LayoutInner {
+                id: id.as_ref().to_string(),
+                x: 0.,
+                y: 0.,
+                pos: Vec2::default(),
+                size: Vec2::default(),
+                fill_w: false,
+                fill_h: false,
+                expand: true, // enable expansion by default
+                mode: LayoutMode::default(),
+                align: Align::default(),
+                spacing: 0.,
+                margins: RectOffset::default(),
+            })),
             layouts: vec![],
+            parent: Option::<SharedLayout>::default(),
         }
     }
 
@@ -89,28 +103,37 @@ impl Layout {
 
     /// Set the layout's id
     pub fn with_id<T: AsRef<str>>(self, id: T) -> Self {
-        Self { id: id.as_ref().to_string(), ..self }
+        self.layout.borrow_mut().id = id.as_ref().to_string();
+        self
     }
 
     /// Set horizontal layout mode
     pub fn with_horz(self) -> Self {
-        Self { mode: LayoutMode::Horizontal, ..self }
+        self.layout.borrow_mut().mode = LayoutMode::Horizontal;
+        self
     }
 
     /// Set vertical layout mode
     pub fn with_vert(self) -> Self {
-        Self { mode: LayoutMode::Vertical, ..self }
+        self.layout.borrow_mut().mode = LayoutMode::Vertical;
+        self
     }
 
     /// Set the layout static position
     pub fn with_pos(self, x: f32, y: f32) -> Self {
-        Self { pos: vec2(x, y), ..self }
+        self.layout.borrow_mut().pos = vec2(x, y);
+        self
     }
 
     /// Set the layout size to full screen
     /// * disables layout expansion
     pub fn with_size_f(self) -> Self {
-        Self { expand: false, size: screen(), ..self }
+        {
+            let layout = &mut *self.layout.borrow_mut();
+            layout.expand = false;
+            layout.size = screen();
+        }
+        self
     }
 
     /// Set the layout size to a percentage of the parent layout
@@ -119,72 +142,102 @@ impl Layout {
     /// * `width` is a percentage of the screen/parent width range of (0.01 - 1.0)
     /// * `height` is a percentage of the screen/parent height range of (0.01 - 1.0)
     pub fn with_size_p(self, width: f32, height: f32) -> Self {
-        //let size = if let Some(parent) = &self.parent { parent.borrow().size } else { screen() };
-        let size = screen();
-        Self { expand: false, size: vec2(size.x * width, size.y * height), ..self }
+        {
+            let size = if let Some(parent) = &self.parent { parent.borrow().size } else { screen() };
+            let layout = &mut *self.layout.borrow_mut();
+            layout.expand = false;
+            layout.size = vec2(size.x * width, size.y * height);
+        }
+        self
     }
 
     /// Set the layout size to a static size
     /// * disables layout expansion
     pub fn with_size_s(self, width: f32, height: f32) -> Self {
-        Self { expand: false, size: vec2(width, height), ..self }
+        {
+            let layout = &mut *self.layout.borrow_mut();
+            layout.expand = false;
+            layout.size = vec2(width, height);
+        }
+        self
     }
 
     /// Fill the entire layout
     pub fn with_fill(self) -> Self {
-        Self { fill_w: true, fill_h: true, ..self }
+        {
+            let layout = &mut *self.layout.borrow_mut();
+            layout.fill_w = true;
+            layout.fill_h = true;
+        }
+        self
     }
 
     /// Fill the entire width of the layout
     pub fn with_fill_w(self) -> Self {
-        Self { fill_w: true, ..self }
+        {
+            let layout = &mut *self.layout.borrow_mut();
+            layout.fill_w = true;
+        }
+        self
     }
 
     /// Fill the entire height of the layout
     pub fn with_fill_h(self) -> Self {
-        Self { fill_h: true, ..self }
+        {
+            let layout = &mut *self.layout.borrow_mut();
+            layout.fill_h = true;
+        }
+        self
     }
 
     /// Configure layout expansion
     /// * When enabled disables fill properties
     pub fn with_expand(self) -> Self {
-        Self { expand: true, fill_h: false, fill_w: false, ..self }
+        {
+            let layout = &mut *self.layout.borrow_mut();
+            layout.expand = true;
+            layout.fill_w = false;
+            layout.fill_h = false;
+        }
+        self
     }
 
     /// Space to allocate between widgets
     pub fn with_spacing(self, spacing: f32) -> Self {
-        Self { spacing, ..self }
+        self.layout.borrow_mut().spacing = spacing;
+        self
     }
 
     /// Space reserved outside the boundaries of the layout
     pub fn with_margins(self, left: f32, right: f32, top: f32, bottom: f32) -> Self {
-        Self { margins: RectOffset { left, right, top, bottom }, ..self }
+        self.layout.borrow_mut().margins = RectOffset { left, right, top, bottom };
+        self
     }
 
     /// Space reserved outside the boundaries of the layout
     pub fn with_margins_p(self, margins: RectOffset) -> Self {
-        Self { margins, ..self }
-    }
-
-    /// Add a parent layout to the layout for relative positioning
-    pub fn with_parent(self, parent: &Layout) -> Self {
-        //Self { parent: Some(Rc::new(RefCell::new(parent))), ..self }
+        self.layout.borrow_mut().margins = margins;
         self
     }
 
+    /// Add a parent layout for relative alignment
+    pub fn with_parent(self, parent: SharedLayout) -> Self {
+        Self { parent: Some(parent), ..self }
+    }
+
     /// Get the layout's current margins
-    pub fn get_margins(&self) -> &RectOffset {
-        &self.margins
+    pub fn get_margins(&self) -> RectOffset {
+        self.layout.borrow().margins
     }
 
     /// Get sub-layout by id
     pub fn get_layout(&self, id: &str) -> Option<&Layout> {
-        self.layouts.iter().find(|x| x.id == id)
+        self.layouts.iter().find(|x| x.layout.borrow().id == id)
     }
 
     /// Get mutable sub-layout by id
     pub fn get_layout_mut(&mut self, id: &str) -> Option<&mut Layout> {
-        self.layouts.iter_mut().find(|x| x.id == id)
+        self.layouts.iter_mut().find(|x| x.layout.borrow().id == id)
     }
 
     /// Get the sub-layout's content position and size by id
@@ -205,26 +258,26 @@ impl Layout {
 
     /// Get the layout's content size
     pub fn get_size(&self) -> Vec2 {
-        self.size
+        self.layout.borrow().size
     }
 
     /// Get the layout's content position i.e. position + padding
     pub fn get_pos(&self) -> Vec2 {
-        self.pos
+        self.layout.borrow().pos
     }
 
     /// Set the layout's id
-    pub fn set_id<T: AsRef<str>>(&mut self, id: T) {
-        self.id = id.as_ref().to_string();
+    pub fn set_id<T: AsRef<str>>(&self, id: T) {
+        self.layout.borrow_mut().id = id.as_ref().to_string();
     }
 
     /// Set the layout's size
-    pub fn set_size_s(&mut self, size: Vec2) {
-        self.size = size;
+    pub fn set_size_s(&self, size: Vec2) {
+        self.layout.borrow_mut().size = size;
     }
 
     /// Set the sub-layout's size by id
-    /// * same as `set_layout_size_p` but takes floats params instead of Vec2
+    /// * same as `set_layout_size_p` but takes float params instead of Vec2
     pub fn set_layout_size_s(&mut self, id: &str, width: f32, height: f32) {
         self.get_layout_mut(id).map(|x| x.set_size_s(vec2(width, height)));
     }
@@ -237,7 +290,7 @@ impl Layout {
 
     /// Set sub-layout by id
     pub fn set_layout(&mut self, layout: Layout) {
-        if let Some(i) = self.layouts.iter().position(|x| x.id == layout.id) {
+        if let Some(i) = self.layouts.iter().position(|x| x.layout.borrow().id == layout.layout.borrow().id) {
             self.layouts[i] = layout;
         } else {
             self.layouts.push(layout);
@@ -246,32 +299,40 @@ impl Layout {
 
     /// Set the size of the layout based on a calculation of total sub-layout size
     pub fn calc_size(&mut self) {
-        self.x = 0.;
-        self.y = 0.;
-        for layout in self.layouts.iter() {
+        let inner = &mut *self.layout.borrow_mut();
+
+        inner.x = 0.;
+        inner.y = 0.;
+        for x in self.layouts.iter() {
+            let layout = &*x.layout.borrow();
             let layout_width = layout.size.x + layout.margins.left + layout.margins.right;
             let layout_height = layout.size.y + layout.margins.top + layout.margins.bottom;
-            match self.mode {
+            match inner.mode {
                 LayoutMode::Horizontal => {
-                    self.x += layout_width;
-                    if self.y < layout_height {
-                        self.y = layout_height;
+                    inner.x += layout_width;
+                    if inner.y < layout_height {
+                        inner.y = layout_height;
                     }
                 },
                 LayoutMode::Vertical => {
-                    if self.x < layout_width {
-                        self.x = layout_width;
+                    if inner.x < layout_width {
+                        inner.x = layout_width;
                     }
-                    self.y += layout_height;
+                    inner.y += layout_height;
                 },
             }
         }
-        self.size = vec2(self.x, self.y);
+        inner.size = vec2(inner.x, inner.y);
     }
 
     /// Create a new layout inside this layout
     pub fn alloc<T: AsRef<str>>(&mut self, id: T, size: Option<Vec2>) -> &Layout {
-        let mut layout = Layout::new(id.as_ref().to_string()).with_parent(&self);
+        let mut layout = Layout::new(id.as_ref().to_string());
+
+        // Set parent if it exists
+        if let Some(parent) = &self.parent {
+            layout = layout.with_parent(parent.clone());
+        }
 
         // If size is given set and re-set expand
         if let Some(size) = size {
