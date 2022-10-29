04d2e5a18899a14f2caadaca60e0057ae7c659d5
phR0ze <prc451@gmail.com>
2022-10-29 10:12:51 -0600

    Moving sub-layouts into interior mutability

--- macroquad_ui/core/src/button.rs
+++ macroquad_ui/core/src/button.rs
@@ -10,36 +10,40 @@ use crate::prelude::*;
 const ICON_ID: &'static str = "icon";
 const LABEL_ID: &'static str = "label";
 
-/// ButtonBuilder provides the ability to preserve widget configuration and be able to repeatedly
-/// create new widget instances based on this configuration rather than have to configure each
-/// individual widget instance.
 #[derive(Debug, Clone)]
-pub struct ButtonBuilder {
-    layout: Layout, // layout for the button
+pub struct Button {
+    dirty: bool,        // track if a skin update is needed
+    skin: Option<Skin>, // skin to use for the entry titles
+    label: String,      // button label text value
+    clicked: bool,      // track button clicked state
+    activated: bool,    // track button activation i.e. odd clicks
 
-    // Background properties
+    // Configuration
+    layout: Layout,
     background: Option<Image>,           // background image to use
     background_clk: Option<Image>,       // background image to use when clicked
     background_hov: Option<Image>,       // background image to use when hovered
     background_color: Color,             // background color
     background_color_clk: Option<Color>, // background color when clicked
     background_color_hov: Option<Color>, // background color when hovered
-
-    // Font properties
     label_font: Option<&'static [u8]>,   // font to use for label
     label_font_size: f32,                // font size to use for label
     label_font_color: Color,             // font color to use for label
     label_font_color_clk: Option<Color>, // font color to use for label when clicked
     label_font_color_hov: Option<Color>, // font color to use for label when hovered
-
-    // Icon properties
-    icon: Option<Texture2D>, // optional icon to display
+    icon: Option<Texture2D>,             // optional icon to display
 }
 
-impl ButtonBuilder {
-    /// Create a new builder instance
-    pub fn new() -> Self {
+/// Button encapsulates and extends Macroquad's button
+impl Button {
+    /// Create a new standard button instance
+    pub fn new<T: AsRef<str>>(label: T) -> Self {
         let mut button = Self {
+            dirty: true,
+            skin: None,
+            label: label.as_ref().to_string(),
+            clicked: false,
+            activated: false,
             layout: Layout::new(""),
             background: None,
             background_clk: None,
@@ -57,93 +61,7 @@ impl ButtonBuilder {
 
         // Add the label layout by default
         button.layout.append_sub(LABEL_ID, None);
-
         button
-    }
-
-    /// Set background image to use
-    pub fn background<T: Into<Option<Image>>>(self, image: T) -> Self {
-        Self { background: image.into(), ..self }
-    }
-
-    /// Set background image to use
-    pub fn background_clk<T: Into<Option<Image>>>(self, image: T) -> Self {
-        Self { background_clk: image.into(), ..self }
-    }
-
-    /// Set background image to use
-    pub fn background_hov<T: Into<Option<Image>>>(self, image: T) -> Self {
-        Self { background_hov: image.into(), ..self }
-    }
-
-    /// Set the background color used for the button
-    pub fn background_color(self, color: Color) -> Self {
-        Self { background_color: color, ..self }
-    }
-
-    /// Set font to use
-    pub fn label_font(self, font: Option<&'static [u8]>) -> Self {
-        Self { label_font: font, ..self }
-    }
-
-    /// Set font size to use for the button label
-    /// * handles scaling for mobile
-    pub fn label_font_size(self, size: f32) -> Self {
-        Self { label_font_size: scale(size), ..self }
-    }
-
-    /// Set font color to use
-    pub fn label_font_color(self, color: Color) -> Self {
-        Self { label_font_color: color, ..self }
-    }
-
-    /// Set label layout to use
-    pub fn label_layout<F: FnOnce(Layout) -> Layout>(mut self, f: F) -> Self {
-        self.layout.set_sub(f(self.layout.sub(LABEL_ID).unwrap().clone()));
-        self
-    }
-
-    /// Set icon to use
-    pub fn icon<T: Into<Option<Texture2D>>>(mut self, icon: T) -> Self {
-        self.layout.prepend_sub(ICON_ID, None);
-        Self { icon: icon.into(), ..self }
-    }
-
-    /// Update icon layout properties
-    pub fn icon_layout<F: FnOnce(Layout) -> Layout>(mut self, f: F) -> Self {
-        self.layout.set_sub(f(self.layout.sub(ICON_ID).unwrap().clone()));
-        self
-    }
-
-    /// Create a new widget instance from this builder
-    pub fn build<T: AsRef<str>>(&self, label: T) -> Button {
-        let conf = self.clone();
-        Button {
-            dirty: true,
-            skin: None,
-            conf: Self { layout: conf.layout.with_id(label.as_ref()), ..conf },
-            label: label.as_ref().to_string(),
-            clicked: false,
-            activated: false,
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct Button {
-    dirty: bool,         // track if a skin update is needed
-    skin: Option<Skin>,  // skin to use for the entry titles
-    conf: ButtonBuilder, // configuration of the button
-    label: String,       // button label text value
-    clicked: bool,       // track button clicked state
-    activated: bool,     // track button activation i.e. odd clicks
-}
-
-/// Button encapsulates and extends Macroquad's button
-impl Button {
-    /// Create a new standard button instance
-    pub fn new<T: AsRef<str>>(label: T) -> Self {
-        ButtonBuilder::new().build(label)
     }
 
     /// Create a new button instance with an icon
@@ -159,53 +77,56 @@ impl Button {
 
     /// Set background image to use
     pub fn with_background<T: Into<Option<Image>>>(self, image: T) -> Self {
-        Button { dirty: true, conf: self.conf.background(image), ..self }
+        Button { dirty: true, background: image.into(), ..self }
     }
 
     /// Set background image to use
     pub fn with_background_clk<T: Into<Option<Image>>>(self, image: T) -> Self {
-        Button { dirty: true, conf: self.conf.background_clk(image), ..self }
+        Button { dirty: true, background_clk: image.into(), ..self }
     }
 
     /// Set background image to use
     pub fn with_background_hov<T: Into<Option<Image>>>(self, image: T) -> Self {
-        Button { dirty: true, conf: self.conf.background_hov(image), ..self }
+        Button { dirty: true, background_hov: image.into(), ..self }
     }
 
     /// Set the background color used for the button
     pub fn with_background_color(self, color: Color) -> Self {
-        Button { dirty: true, conf: self.conf.background_color(color), ..self }
+        Button { dirty: true, background_color: color, ..self }
     }
 
     /// Set font to use
     pub fn with_label_font(self, font: Option<&'static [u8]>) -> Self {
-        Button { dirty: true, conf: self.conf.label_font(font), ..self }
+        Button { dirty: true, label_font: font, ..self }
     }
 
     /// Set font size to use for the button label
     /// * handles scaling for mobile
     pub fn with_label_font_size(self, size: f32) -> Self {
-        Button { dirty: true, conf: self.conf.label_font_size(size), ..self }
+        Button { dirty: true, label_font_size: size, ..self }
     }
 
     /// Set font color to use
     pub fn with_label_font_color(self, color: Color) -> Self {
-        Button { dirty: true, conf: self.conf.label_font_color(color), ..self }
+        Button { dirty: true, label_font_color: color, ..self }
     }
 
     /// Set label layout to use
     pub fn with_label_layout<F: FnOnce(Layout) -> Layout>(self, f: F) -> Self {
-        Button { dirty: true, conf: self.conf.label_layout(f), ..self }
+        //self.layout.set_sub(f(self.layout.sub(LABEL_ID).unwrap().clone()));
+        Button { dirty: true, ..self }
     }
 
     /// Set icon to use
     pub fn with_icon<T: Into<Option<Texture2D>>>(self, icon: T) -> Self {
-        Button { dirty: true, conf: self.conf.icon(icon), ..self }
+        self.layout.prepend_sub(ICON_ID, None);
+        Button { dirty: true, icon: icon.into(), ..self }
     }
 
     /// Update icon layout properties
     pub fn with_icon_layout<F: FnOnce(Layout) -> Layout>(self, f: F) -> Self {
-        Button { dirty: true, conf: self.conf.icon_layout(f), ..self }
+        //self.layout.set_sub(f(self.layout.sub(ICON_ID).unwrap().clone()));
+        Button { dirty: true, ..self }
     }
 
     /// Button label
@@ -232,14 +153,14 @@ impl Button {
         }
         // Create the label style
         let mut style =
-            ui.style_builder().text_color(self.conf.label_font_color).font_size(self.conf.label_font_size as u16);
-        if let Some(color) = self.conf.label_font_color_clk {
+            ui.style_builder().text_color(self.label_font_color).font_size(self.label_font_size as u16);
+        if let Some(color) = self.label_font_color_clk {
             style = style.text_color_clicked(color);
         }
-        if let Some(color) = self.conf.label_font_color_hov {
+        if let Some(color) = self.label_font_color_hov {
             style = style.text_color_hovered(color);
         }
-        if let Some(font) = self.conf.label_font {
+        if let Some(font) = self.label_font {
             style = style.font(font).unwrap();
         }
         let label_style = style.build();
@@ -247,22 +168,22 @@ impl Button {
         // Create the button style
         style = ui
             .style_builder()
-            .color(self.conf.background_color)
-            .color_clicked(self.conf.background_color)
-            .color_hovered(self.conf.background_color);
-        if let Some(background) = &self.conf.background {
+            .color(self.background_color)
+            .color_clicked(self.background_color)
+            .color_hovered(self.background_color);
+        if let Some(background) = &self.background {
             style = style.background(background.clone());
         }
-        if let Some(background) = &self.conf.background_clk {
+        if let Some(background) = &self.background_clk {
             style = style.background_clicked(background.clone());
         }
-        if let Some(background) = &self.conf.background_hov {
+        if let Some(background) = &self.background_hov {
             style = style.background_hovered(background.clone());
         }
-        if let Some(color) = &self.conf.background_color_clk {
+        if let Some(color) = &self.background_color_clk {
             style = style.color_clicked(*color);
         }
-        if let Some(color) = &self.conf.background_color_hov {
+        if let Some(color) = &self.background_color_hov {
             style = style.color_hovered(*color);
         }
         let button_style = style.build();
@@ -272,11 +193,11 @@ impl Button {
 
         // Calculate and cache button component sizes to reduce compute time
         let label_size = text_size(ui, &skin, Some(&self.label));
-        if let Some(_) = &self.conf.icon {
-            self.conf.layout.set_sub_size_s(ICON_ID, label_size.y + 5.0, label_size.y + 5.0);
+        if let Some(_) = &self.icon {
+            self.layout.set_sub_size_s(ICON_ID, label_size.y + 5.0, label_size.y + 5.0);
         }
-        self.conf.layout.set_sub_size_p(LABEL_ID, label_size);
-        self.conf.layout.update();
+        self.layout.set_sub_size_p(LABEL_ID, label_size);
+        self.layout.update();
 
         self.skin = Some(skin);
         self.dirty = false;
@@ -291,21 +212,21 @@ impl Button {
         self.clicked = false; // reset clicked
 
         // Draw button
-        //self.conf.layout.set_pos(pos.x, pos.y);
-        let (pos, size) = self.conf.layout.shape();
+        //self.layout.set_pos(pos.x, pos.y);
+        let (pos, size) = self.layout.shape();
         if widgets::Button::new("").size(size).position(pos).ui(ui) {
             self.activated = !self.activated;
             self.clicked = true;
         }
 
         // Draw icon
-        if let Some(icon) = &self.conf.icon {
-            let (icon_pos, icon_size) = self.conf.layout.sub_shape(ICON_ID).unwrap();
+        if let Some(icon) = &self.icon {
+            let (icon_pos, icon_size) = self.layout.sub_shape(ICON_ID).unwrap();
             widgets::Texture::new(*icon).size(icon_size.x, icon_size.y).position(icon_pos).ui(ui);
         }
 
         // Draw label
-        let (label_pos, label_size) = self.conf.layout.sub_shape(LABEL_ID).unwrap();
+        let (label_pos, label_size) = self.layout.sub_shape(LABEL_ID).unwrap();
         widgets::Label::new(self.label.as_str()).size(label_size).position(label_pos).ui(ui);
 
         ui.pop_skin();
--- macroquad_ui/core/src/layout.rs
+++ macroquad_ui/core/src/layout.rs
@@ -34,7 +34,7 @@
 use crate::prelude::*;
 use std::{cell::RefCell, rc::Rc};
 
-/// Convenience type for shared interior mutable inner Layout object
+/// SharedLayout defines a sharable interior mutable Layout object
 pub type SharedLayout = Rc<RefCell<LayoutInner>>;
 
 // Internal implemenation detail for sharing ownership of layouts
@@ -51,6 +51,7 @@ pub struct LayoutInner {
     mode: LayoutMode,             // layout mode directive
     spacing: f32,                 // space to include between widgets
     margins: RectOffset,          // space outside the frame edge
+    layouts: Vec<SharedLayout>,   // sub-layouts
     parent: Option<SharedLayout>, // parent layout
 }
 
@@ -68,6 +69,7 @@ impl LayoutInner {
             align: Option::<Align>::default(),
             spacing: 0.,
             margins: RectOffset::default(),
+            layouts: vec![],
             parent: Option::<SharedLayout>::default(),
         }))
     }
@@ -118,16 +120,13 @@ impl LayoutInner {
 /// widget should draw itself inside that region of space. Layout region space allocated to widgets
 /// is then tracked.
 #[derive(Clone, Debug, PartialEq)]
-pub struct Layout {
-    inner: SharedLayout,  // layout
-    layouts: Vec<Layout>, // child layouts
-}
+pub struct Layout(SharedLayout);
 
 // Layout constructors and builder functions
 impl Layout {
     /// Create the default layout
     pub fn new<T: AsRef<str>>(id: T) -> Self {
-        Self { inner: LayoutInner::new(id), layouts: vec![] }
+        Self(LayoutInner::new(id))
     }
 
     /// Create the default root layout filling the entire screen
@@ -147,14 +146,14 @@ impl Layout {
 
     /// Set the layout's id
     pub fn with_id<T: AsRef<str>>(self, id: T) -> Self {
-        self.inner.borrow_mut().id = id.as_ref().to_string();
+        self.0.borrow_mut().id = id.as_ref().to_string();
         self
     }
 
     /// Set layout alignment
     pub fn with_align(self, align: Align) -> Self {
         {
-            let inner = &mut *self.inner.borrow_mut();
+            let inner = &mut *self.0.borrow_mut();
             inner.dirty = true;
             inner.align = Some(align);
         }
@@ -164,7 +163,7 @@ impl Layout {
     /// Set horizontal layout mode
     pub fn with_horz(self) -> Self {
         {
-            let inner = &mut *self.inner.borrow_mut();
+            let inner = &mut *self.0.borrow_mut();
             inner.dirty = true;
             inner.mode = LayoutMode::Horizontal;
         }
@@ -174,7 +173,7 @@ impl Layout {
     /// Set vertical layout mode
     pub fn with_vert(self) -> Self {
         {
-            let inner = &mut *self.inner.borrow_mut();
+            let inner = &mut *self.0.borrow_mut();
             inner.dirty = true;
             inner.mode = LayoutMode::Vertical;
         }
@@ -185,7 +184,7 @@ impl Layout {
     /// * disables layout expansion
     pub fn with_size_f(self) -> Self {
         {
-            let inner = &mut *self.inner.borrow_mut();
+            let inner = &mut *self.0.borrow_mut();
             inner.dirty = true;
             inner.expand = false;
             inner.size = screen();
@@ -200,9 +199,8 @@ impl Layout {
     /// * `height` is a percentage of the screen/parent height range of (0.01 - 1.0)
     pub fn with_size_p(self, width: f32, height: f32) -> Self {
         {
-            let size =
-                if let Some(parent) = &self.inner.borrow().parent { parent.borrow().size } else { screen() };
-            let inner = &mut *self.inner.borrow_mut();
+            let size = if let Some(parent) = &self.0.borrow().parent { parent.borrow().size } else { screen() };
+            let inner = &mut *self.0.borrow_mut();
             inner.dirty = true;
             inner.expand = false;
             inner.size = vec2(size.x * width, size.y * height);
@@ -214,7 +212,7 @@ impl Layout {
     /// * disables layout expansion
     pub fn with_size_s(self, width: f32, height: f32) -> Self {
         {
-            let inner = &mut *self.inner.borrow_mut();
+            let inner = &mut *self.0.borrow_mut();
             inner.dirty = true;
             inner.expand = false;
             inner.size = vec2(width, height);
@@ -225,7 +223,7 @@ impl Layout {
     /// Fill the entire layout
     pub fn with_fill(self) -> Self {
         {
-            let inner = &mut *self.inner.borrow_mut();
+            let inner = &mut *self.0.borrow_mut();
             inner.dirty = true;
             inner.fill_w = true;
             inner.fill_h = true;
@@ -236,7 +234,7 @@ impl Layout {
     /// Fill the entire width of the layout
     pub fn with_fill_w(self) -> Self {
         {
-            let inner = &mut *self.inner.borrow_mut();
+            let inner = &mut *self.0.borrow_mut();
             inner.dirty = true;
             inner.fill_w = true;
         }
@@ -246,7 +244,7 @@ impl Layout {
     /// Fill the entire height of the layout
     pub fn with_fill_h(self) -> Self {
         {
-            let inner = &mut *self.inner.borrow_mut();
+            let inner = &mut *self.0.borrow_mut();
             inner.dirty = true;
             inner.fill_h = true;
         }
@@ -257,7 +255,7 @@ impl Layout {
     /// * When enabled disables fill properties
     pub fn with_expand(self) -> Self {
         {
-            let inner = &mut *self.inner.borrow_mut();
+            let inner = &mut *self.0.borrow_mut();
             inner.dirty = true;
             inner.expand = true;
             inner.fill_w = false;
@@ -269,7 +267,7 @@ impl Layout {
     /// Space to allocate between widgets
     pub fn with_spacing(self, spacing: f32) -> Self {
         {
-            let inner = &mut *self.inner.borrow_mut();
+            let inner = &mut *self.0.borrow_mut();
             inner.dirty = true;
             inner.spacing = spacing;
         }
@@ -279,7 +277,7 @@ impl Layout {
     /// Space reserved outside the boundaries of the layout
     pub fn with_margins(self, left: f32, right: f32, top: f32, bottom: f32) -> Self {
         {
-            let inner = &mut *self.inner.borrow_mut();
+            let inner = &mut *self.0.borrow_mut();
             inner.dirty = true;
             inner.margins = RectOffset { left, right, top, bottom };
         }
@@ -289,7 +287,7 @@ impl Layout {
     /// Space reserved outside the boundaries of the layout
     pub fn with_margins_p(self, margins: RectOffset) -> Self {
         {
-            let inner = &mut *self.inner.borrow_mut();
+            let inner = &mut *self.0.borrow_mut();
             inner.dirty = true;
             inner.margins = margins;
         }
@@ -300,7 +298,7 @@ impl Layout {
     /// * when align is set the LayoutMode won't take affect
     pub fn with_parent(self, parent: SharedLayout) -> Self {
         {
-            let inner = &mut *self.inner.borrow_mut();
+            let inner = &mut *self.0.borrow_mut();
             inner.dirty = true;
             inner.parent = Some(parent);
         }
@@ -312,32 +310,32 @@ impl Layout {
 impl Layout {
     /// Get the layout's margins
     pub fn margins(&self) -> RectOffset {
-        self.inner.borrow().margins
+        self.0.borrow().margins
     }
 
     /// Get parent layout's position and size
     /// * position accounts for margins
     /// * returns (pos, size)
     pub fn parent_shape(&self) -> (Vec2, Vec2) {
-        self.inner.borrow().parent_shape()
+        self.0.borrow().parent_shape()
     }
 
     /// Get layout's position and size
     /// * position accounts for margins
     /// * returns (pos, size)
     pub fn shape(&self) -> (Vec2, Vec2) {
-        self.inner.borrow().get_shape()
+        self.0.borrow().get_shape()
     }
 
     /// Get sub-layout by id
-    pub fn sub(&self, id: &str) -> Option<&Layout> {
-        self.layouts.iter().find(|x| x.inner.borrow().id == id)
+    pub fn sub(&self, id: &str) -> Option<Layout> {
+        self.0.borrow().layouts.iter().find(|x| x.borrow().id == id).map(|x| Layout(x.clone()))
     }
 
-    /// Get mutable sub-layout by id
-    pub fn sub_mut(&mut self, id: &str) -> Option<&mut Layout> {
-        self.layouts.iter_mut().find(|x| x.inner.borrow().id == id)
-    }
+    // /// Get mutable sub-layout by id
+    // pub fn sub_mut(self, id: &str) -> Option<Layout> {
+    //     self.0.borrow().layouts.iter().find(|x| x.borrow().id == id)
+    // }
 
     /// Get sub-layout's position and size by id
     /// * position accounts for margins
@@ -349,17 +347,17 @@ impl Layout {
 
     /// Set flag value for triggering a size and position update on next run
     pub fn set_dirty(&self, dirty: bool) {
-        self.inner.borrow_mut().dirty = dirty;
+        self.0.borrow_mut().dirty = dirty;
     }
 
     /// Set the layout's id
     pub fn set_id<T: AsRef<str>>(&self, id: T) {
-        self.inner.borrow_mut().id = id.as_ref().to_string();
+        self.0.borrow_mut().id = id.as_ref().to_string();
     }
 
     /// Set the layout's size
     pub fn set_size_s(&self, size: Vec2) {
-        let inner = &mut *self.inner.borrow_mut();
+        let inner = &mut *self.0.borrow_mut();
         inner.dirty = true;
         inner.expand = false;
         inner.size = size;
@@ -367,29 +365,29 @@ impl Layout {
 
     /// Set sub-layout's size by id
     /// * same as `set_layout_size_p` but takes float params instead of Vec2
-    pub fn set_sub_size_s(&mut self, id: &str, width: f32, height: f32) {
-        self.sub_mut(id).map(|x| x.set_size_s(vec2(width, height)));
+    pub fn set_sub_size_s(&self, id: &str, width: f32, height: f32) {
+        self.sub(id).map(|x| x.set_size_s(vec2(width, height)));
     }
 
     /// Set the sub-layout's size by id
     /// * same as `set_layout_size_s` but takes Vec2 object instead of floats
-    pub fn set_sub_size_p(&mut self, id: &str, size: Vec2) {
-        self.sub_mut(id).map(|x| x.set_size_s(size));
+    pub fn set_sub_size_p(&self, id: &str, size: Vec2) {
+        self.sub(id).map(|x| x.set_size_s(size));
     }
 
     /// Set sub-layout by id
-    pub fn set_sub(&mut self, layout: Layout) {
+    pub fn set_sub(&self, layout: Layout) {
         layout.set_dirty(true);
-        if let Some(i) = self.layouts.iter().position(|x| x.inner.borrow().id == layout.inner.borrow().id) {
-            self.layouts[i] = layout;
+        if let Some(i) = self.0.borrow().layouts.iter().position(|x| x.borrow().id == layout.0.borrow().id) {
+            self.0.borrow_mut().layouts[i] = layout.0.clone();
         } else {
-            self.layouts.push(layout);
+            self.0.borrow_mut().layouts.push(layout.0.clone());
         }
     }
 
     // Create a new layout inside this layout
-    fn alloc_sub<T: AsRef<str>>(&mut self, id: T, size: Option<Vec2>) -> Layout {
-        let mut layout = Layout::new(id.as_ref().to_string()).with_parent(self.inner.clone());
+    fn alloc_sub<T: AsRef<str>>(&self, id: T, size: Option<Vec2>) -> Layout {
+        let mut layout = Layout::new(id.as_ref().to_string()).with_parent(self.0.clone());
 
         // If size is given set the size then re-set expand
         if let Some(size) = size {
@@ -400,28 +398,28 @@ impl Layout {
 
     /// Create a new sub-layout inside this layout
     /// * Adds the new sub-layout to the end of the sub-layout list
-    pub fn append_sub<T: AsRef<str>>(&mut self, id: T, size: Option<Vec2>) -> &Layout {
+    pub fn append_sub<T: AsRef<str>>(&self, id: T, size: Option<Vec2>) -> Layout {
         let layout = self.alloc_sub(id.as_ref(), size);
-        self.layouts.push(layout);
+        self.0.borrow_mut().layouts.push(layout.0.clone());
         self.set_dirty(true);
-        self.sub(id.as_ref()).unwrap()
+        layout
     }
 
     /// Create a new sub-layout inside this layout
     /// * Adds the new sub-layout to the begining of the sub-layout list
-    pub fn prepend_sub<T: AsRef<str>>(&mut self, id: T, size: Option<Vec2>) -> &Layout {
+    pub fn prepend_sub<T: AsRef<str>>(&self, id: T, size: Option<Vec2>) -> Layout {
         let layout = self.alloc_sub(id.as_ref(), size);
-        self.layouts.insert(0, layout);
+        self.0.borrow_mut().layouts.insert(0, layout.0.clone());
         self.set_dirty(true);
-        self.sub(id.as_ref()).unwrap()
+        layout
     }
 
     /// Calculate and set the size and positional offset of the layout and sub-layouts
     /// * only performs calculation if needed
     /// * takes into account margins
     /// * size update has no effect unless expansion is set
-    pub fn update(&mut self) {
-        let inner = &mut *self.inner.borrow_mut();
+    pub fn update(&self) {
+        let inner = &mut *self.0.borrow_mut();
 
         // Bail if no update is needed or expansion is not enabled
         if !inner.dirty || !inner.expand {
@@ -431,8 +429,8 @@ impl Layout {
         // Calculate layout size and set positional offsets along the way
         let mut cursor = Vec2::default(); // track where to start drawing widget inside parent
         let (mut w, mut h) = (0., 0.);
-        for x in self.layouts.iter_mut() {
-            let sub = &mut *x.inner.borrow_mut();
+        for x in inner.layouts.iter_mut() {
+            let sub = &mut *x.borrow_mut();
             sub.pos = cursor; // update positional offset
             let sub_width = sub.size.x + sub.margins.left + sub.margins.right;
             let sub_height = sub.size.y + sub.margins.top + sub.margins.bottom;
