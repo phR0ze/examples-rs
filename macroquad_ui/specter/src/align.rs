e199da74d3dd396ffe451dc74fd85fd55a5a3b29
phR0ze <prc451@gmail.com>
2022-11-07 10:19:16 -0700

    Adding unit tests to code

---
+++ macroquad_ui/specter/src/align.rs
@@ -0,0 +1,113 @@
+//! Align provides functionality for calculating positioning for widgets in their parent layouts
+//!
+use crate::prelude::*;
+
+/// Align is a directive used to guide the calculation of the widgets position in its parent layout
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum Align {
+    /// Align widget in the center horizontally and in the top vertically
+    CenterTop,
+
+    /// Align in the center horizontally and in the center vertically
+    Center,
+
+    /// Align in the center horizontally and in the bottom vertically
+    CenterBottom,
+
+    /// Align in the right horizontally and in the top vertically
+    RightTop,
+
+    /// Align in the right horizontally and in the center vertically
+    RightCenter,
+
+    /// Align in the right horizontally and in the bottom vertically
+    RightBottom,
+
+    /// Align in the left horizontally and in the top vertically
+    LeftTop,
+
+    /// Align in the left horizontally and in the center vertically
+    LeftCenter,
+
+    /// Align in the left horizontally and in the bottom vertically
+    LeftBottom,
+
+    /// Align horizontally with the given value and vertically with the given value
+    Static(f32, f32),
+}
+
+impl Align {
+    /// Returns true if we have a static alignment
+    ///
+    /// ### Examples
+    /// ```
+    /// use specter::prelude::*;
+    ///
+    /// assert_eq!(Align::Center.is_static(), false);
+    /// assert_eq!(Align::Static(0., 0.).is_static(), true);
+    /// ```
+    pub fn is_static(&self) -> bool {
+        match self {
+            Align::Static(_, _) => true,
+            _ => false,
+        }
+    }
+
+    /// Calculate the position vector based on the given widget size and positioning directive as well
+    /// as the containing widget's size and optional position.
+    /// * `size` is the size of the widget to position
+    /// * `cont_size` is the containing widget's size
+    /// * `cont_pos` is the containing positional to offset
+    /// ```
+    /// use specter::prelude::*;
+    ///
+    /// let size = vec2(2., 2.);
+    /// let cont_size = vec2(4., 4.);
+    /// let cont_pos = vec2(2., 2.);
+    /// assert_eq!(Align::LeftTop.relative(size, cont_size, cont_pos), vec2(2., 2.));
+    /// ```
+    pub fn relative(&self, size: Vec2, cont_size: Vec2, cont_pos: Vec2) -> Vec2 {
+        let mut pos = match self {
+            Align::CenterTop => vec2((cont_size.x - size.x) / 2.0, 0.0),
+            Align::Center => vec2(cont_size.x - size.x, cont_size.y - size.y) / 2.0,
+            Align::CenterBottom => vec2((cont_size.x - size.x) / 2.0, cont_size.y - size.y),
+            Align::RightTop => vec2(cont_size.x - size.x, 0.0),
+            Align::RightCenter => vec2(cont_size.x - size.x, (cont_size.y - size.y) / 2.0),
+            Align::RightBottom => vec2(cont_size.x - size.x, cont_size.y - size.y),
+            Align::LeftTop => vec2(0.0, 0.0),
+            Align::LeftCenter => vec2(0.0, (cont_size.y - size.y) / 2.0),
+            Align::LeftBottom => vec2(0.0, cont_size.y - size.y),
+            Align::Static(x, y) => vec2(*x, *y),
+        };
+
+        // If the containing widget's position was given offset by that amount
+        pos.x += cont_pos.x;
+        pos.y += cont_pos.y;
+        pos
+    }
+}
+
+impl Default for Align {
+    fn default() -> Self {
+        Align::LeftTop
+    }
+}
+
+impl From<Vec2> for Align {
+    fn from(val: Vec2) -> Self {
+        Align::Static(val.x, val.y)
+    }
+}
+
+// Unit tests
+// -------------------------------------------------------------------------------------------------
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_foo() {
+        assert_eq!(vec2(2., 2.), vec2(2., 2.));
+    }
+}
--- macroquad_ui/specter/src/lib.rs
+++ macroquad_ui/specter/src/lib.rs
@@ -1,3 +1,50 @@
+//! Immediate mode Ui toolkit
+//!
+//! ### Example
+//! ```
+//! use core::prelude::*;
+//! ```
+// #[macro_use]
+// mod macros;
+
+mod align;
+// mod button;
+// mod fps;
+// mod frame;
+// mod label;
+// mod layout;
+// mod panel;
+// mod utils;
+
+/// All essential symbols in a simple consumable form
+///
+/// ### Examples
+/// ```
+/// use core::prelude::*;
+/// ```
+pub mod prelude {
+    // Re-exports
+    pub use macroquad::{
+        color::colors,
+        prelude::*,
+        ui::{hash, root_ui, widgets, Drag, Id, Skin, Style, Ui},
+    };
+
+    // Export macros by name
+    // pub use crate::id;
+
+    // Export internal types
+    pub use crate::align::*;
+    // pub use crate::button::*;
+    // pub use crate::fps::*;
+    // pub use crate::frame::*;
+    // pub use crate::label::*;
+    // pub use crate::layout::*;
+    // pub use crate::macros::*;
+    // pub use crate::panel::*;
+    // pub use crate::utils::*;
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
