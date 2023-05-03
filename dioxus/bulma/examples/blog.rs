f193f50811e938199ffd8aa5628e1a91fa0abd0d
phR0ze <prc451@gmail.com>
2023-05-02 22:21:17 -0600

    Adding routing and multiple pages

--- dioxus/bulma/examples/blog.rs
+++ dioxus/bulma/examples/blog.rs
@@ -2,8 +2,9 @@
 //!
 
 use bulma::{
+    dioxus_router::{use_router, Router, Route},
     components::{Card, CardContent, CardImage, Navbar},
-    elements::{self, Image, SubTitle, Tag, TagLink, Tags, Title},
+    elements::{Image, SubTitle, Tag, TagLink, Tags, Title},
     icons::*,
     layouts::{Column, Columns, Container},
     prelude::*,
@@ -11,18 +12,18 @@ use bulma::{
 
 fn main() {
     #[cfg(target_family = "wasm")]
-    dioxus_web::launch(App);
+    bulma::dioxus_web::launch(App);
 
     #[cfg(any(windows, unix))]
-    dioxus_desktop::launch_cfg(
+    bulma::dioxus_desktop::launch_cfg(
         App,
-        dioxus_desktop::Config::new().with_window(
-            dioxus_desktop::WindowBuilder::new()
+        bulma::dioxus_desktop::Config::new().with_window(
+            bulma::dioxus_desktop::WindowBuilder::new()
                 .with_title("Bulma Example")
                 .with_resizable(true)
                 //.with_transparent(true)
                 //.with_decorations(false)
-                .with_inner_size(dioxus_desktop::LogicalSize::new(1200, 700)),
+                .with_inner_size(bulma::dioxus_desktop::LogicalSize::new(1200, 700)),
         ),
     )
 }
@@ -32,6 +33,55 @@ fn main() {
 fn App(cx: Scope) -> Element {
     cx.render(rsx! {
         style { "{get_bulma_css()}" },
+        Router {
+            AppHeader {},
+            Route {
+                to: "/"
+                HomePage{}
+            },
+            Route {
+                to: "/posts"
+                PostsPage{}
+            }, 
+            Route {
+                to: ""
+                NotFoundPage{}
+            }, 
+            AppFooter {}
+        }
+    })
+}
+
+#[allow(non_snake_case)]
+fn NotFoundPage(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Page not found"
+        }
+    })
+}
+
+#[allow(non_snake_case)]
+fn AppFooter(cx: Scope) -> Element {
+    cx.render(rsx! {
+        footer {
+            class: "footer",
+            div {
+                class: "content has-text-centered",
+                "Powered by "
+                a { href: "https://dioxuslabs.com/", "Dioxus" }
+                " using "
+                a { href: "https://bulma.io/", "Bulma" }
+                " and images from "
+                a { href: "https://unsplash.com", "Unsplash" }
+            }
+        }
+    })
+}
+
+#[allow(non_snake_case)]
+fn AppHeader(cx: Scope) -> Element {
+    cx.render(rsx! {
         Navbar {
             color: Colors::Info,
             brand: "https://bulma.io/images/bulma-logo.png".into(),
@@ -41,10 +91,16 @@ fn App(cx: Scope) -> Element {
                     class: "navbar-start",
                     a {
                         class: "navbar-item",
+                        onclick: move |_| {
+                            use_router(cx).replace_route("/", None, None)
+                        },
                         "Home"
                     }
                     a {
                         class: "navbar-item",
+                        onclick: move |_| {
+                            use_router(cx).replace_route("/posts", None, None)
+                        },
                         "Posts"
                     }
                     div {
@@ -68,6 +124,95 @@ fn App(cx: Scope) -> Element {
                 }
             }
         }
+    })
+}
+
+#[allow(non_snake_case)]
+fn HomePage(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            class: "tile is-ancestor is-vertical",
+            div {
+                class: "tile is-child hero",
+                div {
+                    class: "hero-body container pb-0",
+                    h1 {
+                        class: "title is-1",
+                        "Welcome..."
+                    },
+                    h2 {
+                        class: "subtitle",
+                        "...to the best yew content"
+                    }
+                },
+                div {
+                    class: "tile is-child",
+                    figure {
+                        class: "image is-3by1",
+                        img {
+                            src: "https://source.unsplash.com/random/1200x400/?yew"
+                        }
+                    }
+                },
+                div {
+                    class: "tile is-parent container",
+                    div {
+                        class: "tile is-parent",
+                        div {
+                            class: "tile is-child box",
+                            p {
+                                class: "title",
+                                "What are yews?"
+                            }
+                            p {
+                                class: "subtitle",
+                                "Everything you need to know!"
+                            }
+                            div {
+                                class: "content",
+                                r#"
+                                A yew is a small to medium-sized evergreen tree, growing 10 to 20 metres tall, with a trunk up to 2 metres in diameter.
+                                The bark is thin, scaly brown, coming off in small flakes aligned with the stem.
+                                The leaves are flat, dark green, 1 to 4 centimetres long and 2 to 3 millimetres broad, arranged spirally on the stem,
+                                but with the leaf bases twisted to align the leaves in two flat rows either side of the stem,
+                                except on erect leading shoots where the spiral arrangement is more obvious.
+                                The leaves are poisonous.
+                                "#
+                            }
+                        }
+                    }
+                    div {
+                        class: "tile is-parent",
+                        div {
+                            class: "tile is-child box",
+                            p {
+                                class: "title",
+                                "Who are we?"
+                            }
+                            div {
+                                class: "content",
+                                "We're a small team of just 2"
+                                sup {
+                                    "64"
+                                }
+                                " members working tirelessly to bring you the low-effort yes conent we all desperately crave."
+                                br {}
+                                r#"
+                                We put a ton of effort into fact-checking our posts.
+                                Some say they read like a Wikipedia article - what a compliment!
+                                "#
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
+#[allow(non_snake_case)]
+fn PostsPage(cx: Scope) -> Element {
+    cx.render(rsx! {
         Container {
             is_fluid: true,
             br {}
--- dioxus/bulma/src/lib.rs
+++ dioxus/bulma/src/lib.rs
@@ -10,6 +10,13 @@ pub mod icons {
     pub use dioxus_free_icons::icons::*;
     pub use dioxus_free_icons::{Icon, IconProps, IconShape};
 }
+pub use dioxus_router;
+
+#[cfg(any(windows, unix))]
+pub use dioxus_desktop;
+
+#[cfg(target_family = "wasm")]
+pub use dioxus_web;
 
 pub mod prelude {
     pub use crate::{get_bulma_css, utils::*};
