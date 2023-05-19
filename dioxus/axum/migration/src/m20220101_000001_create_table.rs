542de4c9a8619ab8cc5589c36b0b946a26a9dbd0
phR0ze <prc451@gmail.com>
2023-05-19 17:01:42 -0600

    Adding initial database structure

--- dioxus/axum/migration/src/m20220101_000001_create_table.rs
+++ dioxus/axum/migration/src/m20220101_000001_create_table.rs
@@ -3,46 +3,166 @@ use sea_orm_migration::prelude::*;
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
+#[derive(Iden)]
+enum User {
+    Table,
+    Id,
+    Name,
+    CreatedAt,
+    ModifiedAt,
+}
+
+#[derive(Iden)]
+enum Category {
+    Table,
+    Id,
+    Name,
+    Value,
+    CreatedAt,
+    ModifiedAt,
+}
+
+/// User has points in a particular category
+#[derive(Iden)]
+enum Points {
+    Table,
+    Id,
+    Value,
+    UserId,
+    CategoryId,
+    CreatedAt,
+    ModifiedAt,
+}
+
+#[derive(Iden)]
+enum Rewards {
+    Table,
+    Id,
+    Value,
+    UserId,
+    CreatedAt,
+    ModifiedAt,
+}
+
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
+        // Create user table
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(User::Table)
                     .if_not_exists()
+                    .col(ColumnDef::new(User::Id).integer().not_null().auto_increment().primary_key())
+                    .col(ColumnDef::new(User::Name).string().not_null())
+                    .col(ColumnDef::new(User::CreatedAt).date_time().not_null().default(Expr::current_timestamp()))
                     .col(
-                        ColumnDef::new(Post::Id)
-                            .integer()
-                            .not_null()
-                            .auto_increment()
-                            .primary_key(),
+                        ColumnDef::new(User::ModifiedAt).date_time().not_null().default(Expr::current_timestamp()),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .to_owned(),
+            )
+            .await?;
+
+        // Create categories table
+        manager
+            .create_table(
+                Table::create()
+                    .table(Category::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(Category::Id).integer().not_null().auto_increment().primary_key())
+                    .col(ColumnDef::new(Category::Name).string().not_null())
+                    .col(ColumnDef::new(Category::Value).integer().not_null())
+                    .col(
+                        ColumnDef::new(Category::CreatedAt)
+                            .date_time()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .col(
+                        ColumnDef::new(Category::ModifiedAt)
+                            .date_time()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        // Create points table
+        manager
+            .create_table(
+                Table::create()
+                    .table(Points::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(Points::Id).integer().not_null().auto_increment().primary_key())
+                    .col(ColumnDef::new(Points::Value).integer().not_null())
+                    .col(ColumnDef::new(Points::UserId).integer().not_null())
+                    .col(ColumnDef::new(Points::CategoryId).integer().not_null())
+                    .col(
+                        ColumnDef::new(Points::CreatedAt)
+                            .date_time()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .col(
+                        ColumnDef::new(Points::ModifiedAt)
+                            .date_time()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk-points-user-id")
+                            .from(Points::Table, Points::UserId)
+                            .to(User::Table, User::Id),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk-points-category-id")
+                            .from(Points::Table, Points::CategoryId)
+                            .to(Category::Table, Category::Id),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        // Create rewards table
+        manager
+            .create_table(
+                Table::create()
+                    .table(Rewards::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(Rewards::Id).integer().not_null().auto_increment().primary_key())
+                    .col(ColumnDef::new(Rewards::Value).integer().not_null())
+                    .col(ColumnDef::new(Rewards::UserId).integer().not_null())
+                    .col(
+                        ColumnDef::new(Rewards::CreatedAt)
+                            .date_time()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .col(
+                        ColumnDef::new(Rewards::ModifiedAt)
+                            .date_time()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk-rewards-user-id")
+                            .from(Rewards::Table, Rewards::UserId)
+                            .to(User::Table, User::Id),
+                    )
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
-        manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
-            .await
+        // Drop the tables in the opposite of creation
+        manager.drop_table(Table::drop().table(Rewards::Table).to_owned()).await?;
+        manager.drop_table(Table::drop().table(Points::Table).to_owned()).await?;
+        manager.drop_table(Table::drop().table(Category::Table).to_owned()).await?;
+        manager.drop_table(Table::drop().table(User::Table).to_owned()).await
     }
-}
-
-/// Learn more at https://docs.rs/sea-query#iden
-#[derive(Iden)]
-enum Post {
-    Table,
-    Id,
-    Title,
-    Text,
 }
