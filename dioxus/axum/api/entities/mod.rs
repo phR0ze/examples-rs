f32e3a16252261273ee109a24350750945c3a146
phR0ze <prc451@gmail.com>
2023-05-19 22:36:45 -0600

    Adding sea generated entities

---
+++ dioxus/axum/api/entities/mod.rs
@@ -0,0 +1,9 @@
+pub mod category;
+pub mod points;
+pub mod rewards;
+pub mod user;
+
+pub use category::Entity as Category;
+pub use points::Entity as Points;
+pub use rewards::Entity as Rewards;
+pub use user::Entity as User;
