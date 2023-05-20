3c51015340ad1ee4f7ac1dae3b1d146ded5f6497
phR0ze <prc451@gmail.com>
2023-05-20 17:17:23 -0600

    Adding frontend to the project

---
+++ dioxus/axum/frontend/src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+    dioxus_logger::init(log::LevelFilter::Info).expect("failed to init logger");
+
+    #[cfg(target_family = "wasm")]
+    bulma::dioxus_web::launch(App);
+
+    #[cfg(any(windows, unix))]
+    bulma::dioxus_desktop::launch_cfg(
+        App,
+        bulma::dioxus_desktop::Config::new().with_window(
+            bulma::dioxus_desktop::WindowBuilder::new()
+                .with_resizable(true)
+                .with_inner_size(bulma::dioxus_desktop::LogicalSize::new(1200, 700)),
+        ),
+    )
+}
+
+// UI entry point that will only get called once on startup
+#[allow(non_snake_case)]
+fn App(cx: Scope) -> Element {
+    fermi::use_init_atom_root(&cx);
+
+    cx.render(rsx! {
+        //style { "{get_bulma_css()}" },
+        // Router {
+        //     pages::Header {},
+        //     pages::Footer {}
+        // }
+        NotFoundPage {}
+    })
+}
+
+#[allow(non_snake_case)]
+fn NotFoundPage(cx: Scope) -> Element {
+    cx.render(rsx! { p { "404 Page not found - Dioxus" } })
+}
